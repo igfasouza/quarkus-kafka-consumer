6157e92f941265ef1e2a2e194b72c362593003f0
Igor Desouza <igfasouza@gmail.com>
2020-09-16 17:50:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quarkus Kafka consumer
+
+This is a simple demo that read a Kafka topic and generate a REST interface with the topic value 
---
+++ src/main/java/com/kafka/demo/KafkaConsumer.java
@@ -0,0 +1,29 @@
+package com.kafka.demo;
+
+import java.util.Optional;
+import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecordMetadata;
+
+@ApplicationScoped
+public class KafkaConsumer {
+
+    @Incoming("mytopic-subscriber")
+    @Outgoing("my-internal-data-stream")
+    @Broadcast
+    public String process(Message<String> incoming) {
+    	String key = getKey(incoming);
+    	return key + ";" + incoming.getPayload();
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private String getKey(Message<String> incoming) {
+		Optional<IncomingKafkaRecordMetadata> metadata = incoming.getMetadata(IncomingKafkaRecordMetadata.class);
+    	IncomingKafkaRecordMetadata<?, ?> record = metadata.orElseThrow(RuntimeException::new);
+    	return record.getKey().toString();
+    }
+	
+}
